11948af6e24d979de77cbd62cc74d3288fe4ea8f
coffee <yafeikou@gmail.com>
2017-10-09 23:01:00 +0800

    添加SGD

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -8,7 +8,7 @@ class LogisticRegression:
     def sigmoid(self,x):
         return 1.0/(1+np.exp(-x))
 
-    def __gradAscend(self,x,y,learning_rate,penalty,lambda_,max_iter,optimizers):
+    def __gradAscend(self,x,y,learning_rate,penalty,lambda_,max_iter,optimizers,batch_size):
         m,n = np.shape(x)
         b = np.ones(m)  
         x = np.mat(np.column_stack((b,x))) #添加偏置b　m*(n+1)
@@ -20,7 +20,11 @@ class LogisticRegression:
                     weigh = weigh + learning_rate*x.T*error  #梯度上升 似然函数求最大值
                 return weigh
             elif optimizers == 'SGD':
-                pass
+                for i in xrange(max_iter):
+                    index = np.random.choice(m,batch_size) #随机抽样batch_size大小　每次迭代只用batch_size大小去更新权重
+                    error = y[index]-self.sigmoid(x[index]*weigh)
+                    weigh = weigh + learning_rate*x[index].T*error
+                return weigh
             else:
                 raise NameError('Just support SGD and BGD')
         elif penalty == 'l2':
@@ -29,10 +33,18 @@ class LogisticRegression:
                     error = y-self.sigmoid(x*weigh)
                     weigh = weigh + learning_rate*(x.T*error + lambda_*weigh/m)
                 return weigh
+            elif optimizers == 'SGD':
+                for i in xrange(max_iter):
+                    index = np.random.choice(m,batch_size) #随机抽样batch_size大小　每次迭代只用batch_size大小去更新权重
+                    error = y[index]-self.sigmoid(x[index]*weigh)
+                    weigh = weigh + learning_rate*(x[index].T*error + lambda_*weigh/m)
+                return weigh
+            else:
+                raise NameError('Just support SGD and BGD')
 
         
 
-    def fit(self,x,y,learning_rate=0.01,penalty=None,lambda_=None,max_iter=100,optimizers='BGD'):
+    def fit(self,x,y,learning_rate=0.01,penalty=None,lambda_=None,max_iter=100,optimizers='BGD',batch_size = None):
         '''
         parameters:
         x:train_data shape:m*n
@@ -43,7 +55,7 @@ class LogisticRegression:
         max_iter:最大迭代次数
         optimizers:优化器 SGD,BGD
         '''
-        self.weigh = self.__gradAscend(x,y,learning_rate,penalty,lambda_,max_iter,optimizers)
+        self.weigh = self.__gradAscend(x,y,learning_rate,penalty,lambda_,max_iter,optimizers,batch_size)
 
     def predict(self,x):
         m,n = x.shape
@@ -74,8 +86,8 @@ if __name__ == '__main__':
     x_test = x[50:]
     y_test = y[50:]
     model = LogisticRegression()
-    model.fit(x_train,y_train,learning_rate=0.01,max_iter=200,penalty='l2',lambda_=2)
-    print model.weigh
+    np.random.seed(45)
+    model.fit(x_train,y_train,learning_rate=0.01,max_iter=300,penalty='l2',lambda_=1,optimizers='SGD',batch_size=40)
     result = model.predict(x_test)
     l = len(result)
     k = 0
